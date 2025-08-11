bae2e4271e7a342fe60d72c91e6f49100bd668b5
Barry Northern <barryjnorthern@gmail.com>
2025-08-11 23:25:41 +0100

    feat(slice): add directive endpoint with envelope and nop publisher (API stub)

--- services/api/main.go
+++ services/api/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/barrynorthern/libretto/services/api/baton"
+	"github.com/barrynorthern/libretto/services/api/publisher"
 )
 
-func healthHandler() http.Handler {
+func healthMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -16,7 +20,16 @@ func healthHandler() http.Handler {
 
 func main() {
 	addr := ":8080"
+	topic := os.Getenv("DIRECTIVE_TOPIC")
+	if topic == "" { topic = "libretto.dev.directive.issued.v1" }
+	producer := os.Getenv("PRODUCER")
+	if producer == "" { producer = "api" }
+
+	mux := healthMux()
+	pub := publisher.NopPublisher{}
+	mux.Handle("/baton/directive", baton.Handler(pub, topic, producer))
+
 	log.Printf("api listening on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, healthHandler()))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
 
